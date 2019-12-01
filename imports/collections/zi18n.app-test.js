f73eb711b896faa71ea80e618f6ce51e0570b79c
daviddews <ddews1@msudenver.edu>
2019-12-01 16:01:03 -0700

    I don't even know anymore

--- imports/collections/zi18n.app-test.js
+++ imports/collections/zi18n.app-test.js
@@ -1,9 +1,17 @@
 import chai from "chai";
+import i18nCollection, {i18n} from "i18n";
+import { Meteor } from "meteor/meteor";
 import { resetDatabase } from "meteor/xolvio:cleaner";
 
-describe.skip("Server side i18n", function() {
+describe.only("Server side i18n", function() {
   beforeEach(function(done) {
     resetDatabase(null, done);
+  });
+
+  it("returns en-us when locale isn't found", function() {
+    chai.assert.doesNotThrow(() => {
+      i18n.localizeMessage("invalid_locale", "i8nvalue");
+    });
   });
 
   it("still needs to be written", function() {
