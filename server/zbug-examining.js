906cebbabfaeb11dfe92f2fcb8340bffe55412d2
david_logan <dave@thisorthat.com>
2019-12-17 08:43:51 -0700

    Adding in test framework to make sure our new game bugs are eliminated.

---
+++ server/zbug-examining.js
@@ -0,0 +1,22 @@
+import chai from "chai";
+
+describe.skip("When starting a game", function() {
+  it("should remove you as an observer from all games when starting a local game", function() {
+    chai.assert.fail("do me ");
+  });
+  it("should remove you as an observer from all games when starting a legacy game", function() {
+    chai.assert.fail("do me ");
+  });
+  it("should remove you as an observer from all games when examining a new game", function() {
+    chai.assert.fail("do me ");
+  });
+});
+
+describe.skip("When logging out", function() {
+  it("should remove you as an observer form all games", function() {
+    chai.assert.fail("do me");
+  });
+  it("should move all games being played to the adjourned collection", function() {
+    chai.assert.fail("do me");
+  });
+});
