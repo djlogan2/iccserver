eb19b492656dad5170a28d2685ff74ced6c57087
david_logan <PE4-fby-o2g-vMN>
2020-04-23 09:14:18 -0600

    OK, I think pgn import finally works correctly.

--- server/pgn/PGNImportStorageAdapter.js
+++ server/pgn/PGNImportStorageAdapter.js
@@ -4,53 +4,69 @@ import { FS } from "meteor/cfs:base-package";
 import { Parser } from "./pgnsigh";
 
 const stream = require("stream");
+const async = require("async");
 const parsers = {};
 
 const GameCollection = new Mongo.Collection("imported_games");
 const TempUploadCollection = new Mongo.Collection("temp_pgn_imports");
 let testme = 0;
 
-function findTempRecord(fileKey) {
+function findTempRecord(fileKey, first) {
   return new Promise((resolve, reject) => {
-    console.log("START findTempRecord");
     TempUploadCollection.rawCollection().findOne({ fileKey: fileKey }, (error, record) => {
+      if(first)
+        record.offset = 0;
       if (error) reject(error);
       else if (!record) reject(new Error("Unable to find temp record"));
       else resolve(record);
-      console.log("END   findTempRecord");
     });
   });
 }
 
 function parseUpToLastNewLine(temp, chunk) {
   return new Promise((resolve, reject) => {
-    console.log("START parseUpToLastNewLine");
     try {
 
+      let start = 0;
+
+      if(temp.offset < temp.size) {
+        if((temp.offset + chunk.length) <= temp.size) {
+          temp.offset += chunk.length;
+          resolve(temp);
+          return;
+        }
+        start = temp.size - temp.offset;
+        temp.offset += chunk.length - start;
+        temp.size += chunk.length - start;
+      } else {
+        temp.size += chunk.length;
+        temp.offset += chunk.length;
+      }
+
       const parser = parsers[temp._id] === undefined ? new Parser() : parsers[temp._id];
+
       if (parsers[temp._id] === undefined)
         parsers[temp._id] = parser;
 
       let end = chunk.lastIndexOf("\n");
 
-      if (end === -1) {
-        temp.string = chunk.toString("utf8");
+      if (end === -1 || end <= start) {
+        temp.string = chunk.toString("utf8", start);
         resolve(temp);
       } else {
         end++;
         if (temp.string) {
           if (!temp.error) {
-            parser.feed(temp.string + chunk.toString("utf8", 0, end));
+            parser.feed(temp.string + chunk.toString("utf8", start, end));
           }
         } else if (!temp.error) {
-          parser.feed(chunk.toString("utf8", 0, end));
+          parser.feed(chunk.toString("utf8", start, end));
         }
       }
       temp.string = chunk.toString("utf8", end);
       temp.gamelist = parser.gamelist;
       delete parser.gamelist;
       resolve(temp);
-      console.log("END   parseUpToLastNewLine");
     } catch (e) {
       reject(e);
     }
@@ -59,7 +75,6 @@ function parseUpToLastNewLine(temp, chunk) {
 
 function finishIfFinished(temp) {
   return new Promise((resolve, reject) => {
-    console.log("START finishIfFinished");
     if (temp.size >= temp.originalsize) {
       if (temp.string) {
         try {
@@ -78,7 +93,6 @@ function finishIfFinished(temp) {
       delete parsers[temp._id];
     }
     resolve(temp);
-    console.log("END   finishIfFinished");
   });
 }
 
@@ -87,135 +101,22 @@ function saveParsedGames(temp) {
     return Promise.resolve(temp);
 
   return new Promise((resolve, reject) => {
-    console.log("START saveParsedGames");
     GameCollection.rawCollection().insertMany(temp.gamelist, (err, res) => {
       delete temp.gamelist;
       if(err) reject(err);
       else resolve(temp);
-      console.log("END   saveParsedGames");
     });
   });
 }
 
 function updateTempRecord(temp) {
   return new Promise((resolve, reject) => {
-    console.log("START updateTempRecord");
     TempUploadCollection.rawCollection().update({_id: temp._id},temp, (err, res) => {
       if(err) reject(err);
       else resolve(temp);
-      console.log("END   updateTempRecord");
       testme = 0;
     });
   });
-}
-
-class MyWriter extends stream.Writable {
-  constructor(fileKey) {
-    console.log("MyWriter()");
-    super({});
-    this.fileKey = fileKey;
-  }
-
-  _write(chunk, encoding, callback) {
-    const self = this;
-    this.pause();
-    console.log("START _write");
-    findTempRecord(this.fileKey)
-      .then((temp) => parseUpToLastNewLine(temp, chunk))
-      .then((temp) => finishIfFinished(temp))
-      .then((temp) => saveParsedGames(temp))
-      .then((temp) => updateTempRecord(temp))
-      .then(() => self.resume())
-      .then(() => callback())
-      .catch((err) => callback(err));
-    console.log("END   _write");
-  }
-
-  _xxxwrite(chunk, encoding, callback) {
-    ///
-    TempUploadCollection.rawCollection().findOne({ fileKey: this.fileKey }, (e0, temp) => {
-      if (e0) {
-        callback(new Error(e0));
-        return;
-      }
-
-      if (!temp)
-        throw new Error("Unable to find temp record for PGNImportAdapter for id " + fileKey);
-
-      console.log("START  MyWriter::_write, chunk=" + chunk.toString("utf8", 0, 100));
-      const parser = parsers[temp._id] === undefined ? new Parser() : parsers[temp._id];
-      if (parsers[temp._id] === undefined)
-        parsers[temp._id] = parser;
-
-      temp.chunks++;
-      temp.size += chunk.length;
-      let end;
-
-      try {
-        end = chunk.lastIndexOf("\n");
-        if (end === -1) {
-          temp.string = chunk.toString("utf8");
-        } else {
-          end++;
-          if (temp.string) {
-            if (!temp.error) {
-              parser.feed(temp.string + chunk.toString("utf8", 0, end));
-            }
-          } else if (!temp.error) {
-            parser.feed(chunk.toString("utf8", 0, end));
-          }
-        }
-        temp.string = chunk.toString("utf8", end);
-      } catch (e) {
-        temp.error = e;
-        temp.error_line = e.toString();
-      } finally {
-
-        if (temp.size >= temp.originalsize) {
-          if (temp.string) {
-            try {
-              parser.feed(temp.string);
-            } catch (e) {
-              temp.error = e;
-              temp.error_line = e.toString();
-            }
-          }
-          temp.complete = true;
-          delete parsers[temp._id];
-        }
-
-        if (parser.gamelist && parser.gamelist.length) {
-          GameCollection.rawCollection().insertMany(parser.gamelist, (e1, r1) => {
-            delete parser.gamelist;
-            if (e1) {
-              if (temp.error_line) temp.error_line += "\n" + e1.toString();
-              else temp.error_line = e1.toString();
-            }
-            TempUploadCollection.rawCollection().update({ _id: temp._id }, temp, (e2, r2) => {
-              if (e2) {
-                if (temp.error_line) temp.error_line += "\n" + e2.toString();
-                else temp.error_line = e2.toString();
-              }
-              if (temp.error_line)
-                callback(new Error(temp.error_line));
-              else callback();
-            });
-          });
-        } else {
-          TempUploadCollection.rawCollection().update({ _id: temp._id }, temp, (e1, r1) => {
-            if (e1) {
-              if (temp.error_line) temp.error_line += "\n" + e1.toString();
-              else temp.error_line = e1.toString();
-            }
-            if (temp.error_line)
-              callback(new Error(temp.error_line));
-            else callback();
-          });
-        }
-      }
-      console.log("END  MyWriter::_write");
-    });
-  }
 }
 
 export const PGNImportStorageAdapter = function() {
@@ -239,6 +140,7 @@ PGNImportStorageAdapter.prototype.fileKey = function(fileObj) {
       chunks: 0,
       originalsize: fileObj.original.size,
       size: 0,
+      offset: 0,
       string: null
     });
   return fileObj._id;
@@ -249,9 +151,44 @@ PGNImportStorageAdapter.prototype.createReadStream = function(fileKey, options) 
   return null;
 };
 
+const q = async.queue(function(work, callback) {
+  let data = false;
+  let ended = false;
+  let first = true;
+
+  work.stream.on('data', function(chunk){
+    this.pause();
+    data = true;
+    const self = this;
+
+    findTempRecord(work.fileKey, first)
+      .then((temp) => parseUpToLastNewLine(temp, chunk))
+      .then((temp) => finishIfFinished(temp))
+      .then((temp) => saveParsedGames(temp))
+      .then((temp) => updateTempRecord(temp))
+      .finally(() => {
+        data = false;
+        first = false;
+        self.resume();
+        if(ended) {
+          callback();
+        }
+      });
+  });
+
+  work.stream.on('end', function(){
+    ended = true;
+    if(!data) {
+      callback();
+    }
+  });
+}, 1);
+
 PGNImportStorageAdapter.prototype.createWriteStream = function(fileKey) {
-  return new MyWriter(fileKey);
-};
+  const pass = new stream.PassThrough();
+  q.push({fileKey: fileKey, stream: pass}, (err) => {});
+  return pass;
+}
 
 PGNImportStorageAdapter.prototype.remove = function(fileKey, callback) {
   console.log("--- REMOVE ---");
