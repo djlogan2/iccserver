1d915b750e2227bfede3c1fad173f911c0044388
DewsDavid31 <ddews1@msudenver.edu>
2020-05-15 11:29:51 -0600

    Bug on my end with searchGameHistory

--- server/zkibitz.app-test.js
+++ server/zkibitz.app-test.js
@@ -7,7 +7,7 @@ import { PublicationCollector } from "meteor/johanbrook:publication-collector";
 import { Meteor } from "meteor/meteor";
 import { standard_member_roles } from "../imports/server/userConstants";
 
-describe("kibitzes", function() {
+describe.only("kibitzes", function() {
   const self = TestHelpers.setupDescribe.apply(this);
   it("should save an action in the game record for local games", function() {
 
