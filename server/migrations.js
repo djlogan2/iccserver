30d4fb21db079c1bc7cd917ebb411859ff7f6fca
david_logan <PE4-fby-o2g-vMN>
2020-10-30 23:34:16 -0600

    Adding a migration script to update ratings based off of the games already played in game_history. Hopefully it works!

--- server/migrations.js
+++ server/migrations.js
@@ -1,6 +1,8 @@
 import { DynamicRatings } from "./DynamicRatings";
 import { Migrations } from "meteor/patelutsav:meteor-migrations";
 import { Logger } from "../lib/server/Logger";
+import { SystemConfiguration } from "../imports/collections/SystemConfiguration";
+import { Game, GameHistory } from "./Game";
 
 const log = new Logger("server/migrations");
 
@@ -37,6 +39,16 @@ Meteor.startup(() => {
     name: "Remove rated/unrated from dynamic_ratings table",
     run: () => {
       DynamicRatings.collection.update({}, { $unset: { rated: 1, unrated: 1 } }, { multi: true });
+    }
+  });
+  Migrations.add({
+    version: "0.2.0_2",
+    name: "Update ratings from game history",
+    run: () => {
+      const games = GameHistory.collection
+        .find({ rated: true }, { sort: { "actions.date": 1 } })
+        .fetch();
+      games.forEach(game => Game.updateUserRatings(game, game.result, game.status));
     }
   });
   Migrations.migrateTo("latest");
