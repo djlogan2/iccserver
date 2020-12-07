493acc4acc5f8f691779cf33275af718a72a4ce2
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2020-12-07 11:33:01 +0200

    fix: added resource constants to renderRoutes function

--- imports/constants/resourceConstants.js
+++ imports/constants/resourceConstants.js
@@ -5,3 +5,4 @@ export const resourceExamine = "/examine";
 export const resourceUploadPgn = "/upload-pgn";
 export const resourceCommunity = "/community";
 export const resourcePlay = "/play";
+export const resourceEditor = "/editor";
