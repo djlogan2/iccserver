825dddb95e26d0fc16352e4f1bd4c80a14208ccb
david_logan <PE4-fby-o2g-vMN>
2020-12-16 11:00:48 -0700

    A quick publication of key client readable system configuration values

---
+++ server/zalbis-requests.app-test.js
@@ -0,0 +1,6 @@
+describe("Games", function(){
+  it("should automatically draw when the number of repetition posistions equals the admin specified variable", function(){
+    // Like SystemConfiguration.automaticDrawByRepetition(5)
+    chai.assert.fail("do me");
+  });
+});
