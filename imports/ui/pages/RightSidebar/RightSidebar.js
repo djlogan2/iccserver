1417bf702ce13edd5ffac1d948f3c73de90af586
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2020-12-17 18:54:02 +0200

    refactor RightSidebar component

--- imports/ui/pages/RightSidebar/RightSidebar.js
+++ imports/ui/pages/RightSidebar/RightSidebar.js
@@ -3,104 +3,108 @@ import RightBarTabs from "./RightBarToptabs";
 import RightBarTopActivetabs from "./RightBarTopActivetabs";
 import RightBarBottom from "./RightBarBottom";
 import RightBarBottomActiveTabs from "./RightBarBottomActiveTabs";
-import RightBarBottomPostGameTabs from "./RightBarBottomPostGameTabs";
-
-import { Logger } from "../../../../lib/client/Logger";
-
-// eslint-disable-next-line no-unused-vars
-const log = new Logger("client/RightSidebar");
 
 class RightSidebar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: true,
       gameRequest: props.gameRequest
     };
   }
 
   componentWillReceiveProps(nextProps) {
-    if (!!this.props.gameRequest) {
-      if (nextProps.gameRequest !== this.props.gameRequest) {
-        this.setState({ gameRequest: this.props.gameRequest });
+    const { gameRequest } = this.props;
+
+    if (!!gameRequest) {
+      if (nextProps.gameRequest !== gameRequest) {
+        this.setState({ gameRequest });
       }
     }
   }
 
   renderTopRigthSidebar = () => {
-    let topTabitem;
-    if (
-      (this.props.gameStatus === "playing" || this.props.gameStatus === "examining") &&
-      this.props.newOppenetRequest === false
-    ) {
-      topTabitem = (
+    const {
+      gameStatus,
+      newOppenetRequest,
+      RightSidebarData,
+      cssManager,
+      flip,
+      startGameExamine,
+      examineAction,
+      gameRequest
+    } = this.props;
+    const { gameRequest: stateGameRequest } = this.state;
+
+    if ((gameStatus === "playing" || gameStatus === "examining") && !newOppenetRequest) {
+      return (
         <RightBarTopActivetabs
-          RightBarTopData={this.props.RightSidebarData}
-          cssManager={this.props.cssManager}
-          flip={this.props.flip}
-          gameRequest={this.state.gameRequest}
-          startGameExamine={this.props.startGameExamine}
-          examineAction={this.props.examineAction}
+          RightBarTopData={RightSidebarData}
+          cssManager={cssManager}
+          flip={flip}
+          gameRequest={stateGameRequest}
+          startGameExamine={startGameExamine}
+          examineAction={examineAction}
           ref="right_bar_top"
         />
       );
     } else {
-      topTabitem = (
+      return (
         <RightBarTabs
-          cssManager={this.props.cssManager}
-          gameRequest={this.props.gameRequest}
-          flip={this.props.flip}
+          cssManager={cssManager}
+          gameRequest={gameRequest}
+          flip={flip}
           ref="right_bar_top"
         />
       );
     }
-    return topTabitem;
   };
 
   renderBottomRightSidebar = () => {
-    let bottomTabitem = null;
-    if (this.props.gameStatus === "examining") {
-      bottomTabitem = (
-        <RightBarBottom
-          cssManager={this.props.cssManager}
-          gameRequest={this.props.gameRequest}
-          clientMessage={this.props.clientMessage}
-          activeTabnumber={this.props.activeTabnumber}
-          Gamedata={this.props.RightSidebarData}
-          uploadPgn={this.props.uploadPgn}
-        />
-      );
-    } else if (this.props.gameStatus === "examining") {
-      bottomTabitem = (
-        <RightBarBottomPostGameTabs
-          cssManager={this.props.cssManager}
-          gameRequest={this.props.gameRequest}
-          clientMessage={this.props.clientMessage}
-          game={this.props.game}
-        />
-      );
-    } else {
-      bottomTabitem = (
-        <RightBarBottomActiveTabs
-          cssManager={this.props.cssManager}
-          gameRequest={this.props.gameRequest}
-          clientMessage={this.props.clientMessage}
-          game={this.props.game}
-        />
-      );
+    const {
+      gameStatus,
+      cssManager,
+      gameRequest,
+      clientMessage,
+      game,
+      activeTabnumber,
+      RightSidebarData,
+      uploadPgn
+    } = this.props;
+
+    switch (gameStatus) {
+      case "examining":
+        return (
+          <RightBarBottom
+            cssManager={cssManager}
+            gameRequest={gameRequest}
+            clientMessage={clientMessage}
+            activeTabnumber={activeTabnumber}
+            Gamedata={RightSidebarData}
+            uploadPgn={uploadPgn}
+          />
+        );
+      default:
+        return (
+          <RightBarBottomActiveTabs
+            cssManager={cssManager}
+            gameRequest={gameRequest}
+            clientMessage={clientMessage}
+            game={game}
+          />
+        );
     }
-    return bottomTabitem;
   };
 
   render() {
+    const { cssManager } = this.props;
+
     return (
       <div className="right-content-desktop">
-        <div style={this.props.cssManager.rightTopContent()}>{this.renderTopRigthSidebar()}</div>
-        <div style={this.props.cssManager.rightBottomContent()}>
-          {this.renderBottomRightSidebar()}
-        </div>
+        <div style={cssManager.rightTopContent()}>{this.renderTopRigthSidebar()}</div>
+        <div style={cssManager.rightBottomContent()}>{this.renderBottomRightSidebar()}</div>
       </div>
     );
   }
 }
+
 export default RightSidebar;
