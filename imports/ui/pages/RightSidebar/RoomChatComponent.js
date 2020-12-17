70246bd34a1fd6369f1a4125edac5ce468e0edf8
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2020-12-17 19:02:05 +0200

    refactor RoomChatComponent component

--- imports/ui/pages/RightSidebar/RoomChatComponent.js
+++ imports/ui/pages/RightSidebar/RoomChatComponent.js
@@ -1,72 +1,19 @@
 import React, { Component } from "react";
+import { chatLabelStyle, chatRooms } from "../../../constants/RoomChatConstants";
 
 export default class RoomChatComponent extends Component {
-  constructor(props) {
-    super(props);
-
-    this.chatRooms = [
-      {
-        roomId: "Room1",
-        Users: "10 Users"
-      },
-      {
-        roomId: "Room2",
-        Users: "10 Users"
-      },
-      {
-        roomId: "Room3",
-        Users: "10 Users"
-      },
-      {
-        roomId: "Room4",
-        Users: "10 Users"
-      },
-      {
-        roomId: "Room5",
-        Users: "10 Users"
-      }
-    ];
-  }
-  getLang() {
-    return (
-      (navigator.languages && navigator.languages[0]) ||
-      navigator.language ||
-      navigator.browserLanguage ||
-      navigator.userLanguage ||
-      "en-US"
-    );
-  }
   render() {
-    let content = [];
-    this.chatRooms.forEach((room, index) => {
-      content.push(
-        <button key={index} style={this.props.cssManager.matchUserButton()}>
-          <label
-            style={{
-              fontWeight: "300",
-              paddingRight: "10px",
-              paddingLeft: "5px",
-              verticalAlign: "top"
-            }}
-          >
-            {room.roomId}
-          </label>
-          <label
-            style={{
-              fontWeight: "300",
-              paddingRight: "10px",
-              paddingLeft: "5px",
-              verticalAlign: "top"
-            }}
-          >
-            {room.Users}
-          </label>
-        </button>
-      );
-    });
+    const { cssManager } = this.props;
+
+    const content = chatRooms.map((room, index) => (
+      <button key={index} style={cssManager.matchUserButton()}>
+        <label style={chatLabelStyle}>{room.roomId}</label>
+        <label style={chatLabelStyle}>{room.Users}</label>
+      </button>
+    ));
     return (
       <div>
-        <div style={this.props.cssManager.subTabHeader()}>{content}</div>
+        <div style={cssManager.subTabHeader()}>{content}</div>
       </div>
     );
   }
