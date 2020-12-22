a56d2c84dda7ae35188f149a9da7d5df79f75874
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2020-12-22 18:37:16 +0200

    start refactoring Play component

--- imports/ui/pages/Play/Play.js
+++ imports/ui/pages/Play/Play.js
@@ -22,6 +22,7 @@ import { compose } from "redux";
 import { withPlayNotifier } from "../../HOCs/withPlayNotifier";
 import injectSheet from "react-jss";
 import { dynamicPlayNotifierStyles } from "./dynamicPlayNotifierStyles";
+import { resourceExamine } from "../../../constants/resourceConstants";
 
 const log = new Logger("client/Play_js");
 
@@ -40,23 +41,15 @@ class Play extends Component {
     this._board = new Chess.Chess();
     this._boardfallensolder = new Chess.Chess();
     this.userpending = null;
+
     this.state = {
       gameType: null,
       gameData: null
     };
-    this.logout = this.logout.bind(this);
-    this.drawCircle = this.drawCircle.bind(this);
-    this.removeCircle = this.removeCircle.bind(this);
   }
 
   userRecord() {
     return mongoUser.find().fetch();
-  }
-
-  componentWillMount() {
-    if (!Meteor.userId()) {
-      this.props.history.push("/login");
-    }
   }
 
   componentDidMount() {
@@ -71,44 +64,29 @@ class Play extends Component {
     }
   }
 
-  logout(e) {
-    e.preventDefault();
-    Meteor.logout(err => {
-      if (err) {
-        log.error("logout error", err.reason);
-      } else {
-        this.props.history.push("/login");
-      }
-    });
-    this.props.history.push("/login");
-  }
+  drawCircle = (square, color, size) => {
+    const { in_game } = this.props;
 
-  drawCircle(square, color, size) {
-    Meteor.call(
-      "drawCircle",
-      "DrawCircle",
-      this.props.in_game._id,
-      square,
-      color,
-      size,
-      handleError
-    );
-  }
-
-  removeCircle(square) {
-    Meteor.call("removeCircle", "RemoveCircle", this.props.in_game._id, square, handleError);
-  }
-
-  _pieceSquareDragStop = raf => {
-    //log.debug("_pieceSquareDragStop", [this.props.in_game._id, raf.move]);
-    Meteor.call("addGameMove", "gameMove", this.props.in_game._id, raf.move, handleError);
+    Meteor.call("drawCircle", "DrawCircle", in_game._id, square, color, size, handleError);
   };
 
-  _boardFromMongoMessages(game) {
-    //logger.debug("_boardFromMongoMessages", game);
+  removeCircle = square => {
+    const { in_game } = this.props;
+
+    Meteor.call("removeCircle", "RemoveCircle", in_game._id, square, handleError);
+  };
+
+  _pieceSquareDragStop = raf => {
+    const { in_game } = this.props;
+
+    Meteor.call("addGameMove", "gameMove", in_game._id, raf.move, handleError);
+  };
+
+  _boardFromMongoMessages = game => {
     if (!game.fen) {
       return;
     }
+
     let variation = game.variations;
     this._board.load(game.fen);
     this._boardfallensolder = new Chess.Chess();
@@ -117,14 +95,14 @@ class Play extends Component {
     for (let i = 0; i < variation.cmi; i++) {
       if (itemToBeRemoved.indexOf(i) === -1) {
         const moveListItem = variation.movelist[i];
-        if (moveListItem !== undefined) {
+        if (moveListItem) {
           const variationI = moveListItem.variations;
-          if (variationI !== undefined) {
+          if (variationI) {
             const len = variationI.length;
-            if (len === 1 && variation.movelist[variationI[0]] !== undefined) {
+            if (len === 1 && variation.movelist[variationI[0]]) {
               this._boardfallensolder.move(variation.movelist[variationI[0]].move);
             } else if (len > 1) {
-              if (variation.movelist[variationI[len - 1]] !== undefined) {
+              if (variation.movelist[variationI[len - 1]]) {
                 this._boardfallensolder.move(variation.movelist[variationI[len - 1]].move);
               }
               if (variation.cmi === variationI[len - 1]) {
@@ -154,11 +132,12 @@ class Play extends Component {
         return accumulator;
       }
     }, position);
-  }
+  };
 
   handleExamine = () => {
-    debugger;
-    this.props.history.push("/examine");
+    const { history } = this.props;
+
+    history.push(resourceExamine);
   };
 
   getCoordinatesToRank(square) {
@@ -186,8 +165,10 @@ class Play extends Component {
   };
 
   handleRematch = () => {
-    if (this.state.gameType === "startBotGame") {
-      let {
+    const { gameType, gameData } = this.state;
+
+    if (gameType === "startBotGame") {
+      const {
         wild_number,
         rating_type,
         white_initial,
@@ -198,7 +179,7 @@ class Play extends Component {
         black_increment_or_delay_type,
         skill_level,
         color
-      } = this.state.gameData;
+      } = gameData;
       this.handleBotPlay(
         wild_number,
         rating_type,
@@ -217,8 +198,9 @@ class Play extends Component {
   };
 
   initFriendRematch = () => {
-    let game = this.props.in_game;
-    let newMatchData = this.genOptions(game);
+    const { in_game } = this.props;
+
+    const newMatchData = this.genOptions(in_game);
     this.handleChooseFriend(newMatchData);
   };
 
@@ -300,10 +282,9 @@ class Play extends Component {
   };
 
   render() {
-    // log.trace("Play render", this.props);
+    const { isReady, game_request, in_game, usersToPlayWith } = this.props;
 
-    if (!this.props.isready) {
-      //  log.error("Play LOADING");
+    if (!isReady) {
       return <Loading/>;
     }
 
@@ -315,11 +296,13 @@ class Play extends Component {
     };
 
     const css = new CssManager(systemCss, boardCss);
-    if (!!this.props.game_request)
-      this.message_identifier = "server:game:" + this.props.game_request._id;
-    if (this.props.in_game) {
-      this.message_identifier = "server:game:" + this.props.in_game._id;
-      capture = this._boardFromMongoMessages(this.props.in_game);
+    if (!!game_request) {
+      this.message_identifier = "server:game:" + game_request._id;
+    }
+
+    if (in_game) {
+      this.message_identifier = "server:game:" + in_game._id;
+      capture = this._boardFromMongoMessages(in_game);
     }
 
     let opponentName;
@@ -328,23 +311,19 @@ class Play extends Component {
     let result;
     let status2;
     const gamemessage = this.clientMessages(this.message_identifier);
-    const visible =
-      !!gamemessage && !!this.props.in_game && this.props.in_game.status === "examining";
+    const visible = !!gamemessage && !!in_game && in_game.status === "examining";
     if (visible) {
-      result = this.props.in_game.result;
-      status2 = this.props.in_game.status2;
+      result = in_game.result;
+      status2 = in_game.status2;
       userColor =
-        this.props.in_game.white.name === !!Meteor.user() && Meteor.user().username
-          ? "white"
-          : "black";
-      if (userColor === undefined) {
-        debugger;
+        in_game.white.name === !!Meteor.user() && Meteor.user().username ? "white" : "black";
+
+      if (!userColor) {
+        log.error("userColor is missing");
       }
 
-      opponentName =
-        userColor === "white" ? this.props.in_game.black.name : this.props.in_game.white.name;
-      opponentId =
-        userColor === "white" ? this.props.in_game.black._id : this.props.in_game.white._id;
+      opponentName = userColor === "white" ? in_game.black.name : in_game.white.name;
+      opponentId = userColor === "white" ? in_game.black._id : in_game.white._id;
     }
 
     return (
@@ -352,23 +331,23 @@ class Play extends Component {
         <PlayModaler
           userColor={userColor}
           visible={visible}
-          userName={!!Meteor.userId() ? Meteor.user().username : "Logged Out"}
           gameResult={result}
           gameStatus2={status2}
           clientMessage={gamemessage}
           opponentName={opponentName}
           opponentId={opponentId}
+          userName={!!Meteor.userId() ? Meteor.user().username : "Logged Out"}
           onRematch={this.handleRematch}
           onExamine={this.handleExamine}
         />
         <PlayPage
           cssManager={css}
+          capture={capture}
+          game={in_game}
+          usersToPlayWith={usersToPlayWith}
           board={this._board}
-          usersToPlayWith={this.props.usersToPlayWith}
           onChooseFriend={this.handleChooseFriend}
           onBotPlay={this.handleBotPlay}
-          capture={capture}
-          game={this.props.in_game}
           onDrop={this._pieceSquareDragStop}
           onDrawObject={this.handleDraw}
           onRemoveCircle={this.removeCircle}
@@ -393,7 +372,7 @@ export default compose(
     };
 
     return {
-      isready: isReadySubscriptions(subscriptions),
+      isReady: isReadySubscriptions(subscriptions),
 
       usersToPlayWith: Meteor.users
         .find({ $and: [{ _id: { $ne: Meteor.userId() } }, { "status.game": { $ne: "playing" } }] })
