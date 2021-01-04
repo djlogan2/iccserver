53e0c718cf1cf251d4c75a6d840822c7dfce6453
Aleksandr Oleshko <oleshko_o@dlit.dp.ua>
2021-01-04 18:13:24 +0200

    https://app.asana.com/0/1199540153229719/1199719212539855 fix: styles with long word at community tab

--- imports/ui/pages/components/Chat/MessageItem.js
+++ imports/ui/pages/components/Chat/MessageItem.js
@@ -4,7 +4,9 @@ export default ({ name, text }) => {
   return (
     <div className="message-item">
       <div className="message-item__name">{name}</div>
-      <p className="message-item__text">{text}</p>
+      <p className="message-item__text" style={{ wordBreak: "break-word" }}>
+        {text}
+      </p>
     </div>
   );
 };
