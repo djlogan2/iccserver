64a4442239be82e11b20f894dd8a5fb78e531543
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-01-28 15:00:28 +0200

    add migrations

--- server/migrations.js
+++ server/migrations.js
@@ -126,6 +126,14 @@ Meteor.startup(() => {
     }
   });
 
+  Migrations.add({
+    version: "0.2.0_12",
+    name: "Update client i18n for en locales v 0.8",
+    run: () => {
+      mongoClientInternationalization.update({ locale: "en-us" }, { $set: { i18n: english } });
+    }
+  });
+
   Migrations.unlock();
 
   Migrations.migrateTo("latest");
