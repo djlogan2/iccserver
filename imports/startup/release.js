1c964857c8629692ed6fd7d3cd50fb1365127e8f
david_logan <PE4-fby-o2g-vMN>
2021-02-24 21:04:48 -0700

    A couple of minor changes that should improve the accuracy of timestamp a little, and also just squish the negative delay error message. No need for it anymore.

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.16", "commit":"70968021b1d882ae6e99da6bd2676f930282ad8a"}
+export const current_release={"release": "release-0.3.17", "commit":"59fb14ec36cb7dc6351927f43e722f528a613b78"}
