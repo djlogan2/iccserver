751ace456e9ba4fec3e09012665fea551c7c6cbf
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-03-01 17:53:55 +0200

    try to minify unused lodash and antd parts of libraries v0.2

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.20", "commit":"b8d49e52083e2446f5a4939c0f2c214c3ea643c5"}
+export const current_release={"release": "release-0.3.21-1-g8cdef7a", "commit":"8cdef7a2fefca807acda56dfe760b36d0a320414"}
