d82b5be78ffafaaa1d4dd87deb444624bac329d3
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-03-26 17:13:23 +0200

    fix: package.json dependencies

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.21-15-g256bc70", "commit":"256bc70020ed9a24984b55853c53d63d67496457"}
+export const current_release={"release": "release-0.3.21-17-gde7552b", "commit":"de7552b39be8bf94ca201974ee42721293fcaa3f"}
