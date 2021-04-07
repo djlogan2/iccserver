ca7d615947186eb61ed9cd5e096a8db46b875c6f
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-04-07 15:48:31 +0300

    lib: update dnd issues of chessboard lib

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.21-26-g351506e", "commit":"351506e0cf7374f7bd2a23d0e21b9cda314d7087"}
+export const current_release={"release": "release-0.3.21-27-ga77d02f", "commit":"a77d02f2c2272daa3a36fe0e0a42d7097454b7a8"}
