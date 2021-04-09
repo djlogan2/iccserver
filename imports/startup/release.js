82ca60dc8c80355f1fc5c997f476973c3fce7d21
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-04-09 15:13:00 +0300

    feat: update lib board version

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.21-28-gca7d615", "commit":"ca7d615947186eb61ed9cd5e096a8db46b875c6f"}
+export const current_release={"release": "release-0.3.21-29-g1f3054a", "commit":"1f3054acf818aab252387da606c9a805618b78ef"}
