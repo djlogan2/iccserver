ad6991e701e34482bb1cc338c46d7d1b11761cd8
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-04-12 17:24:09 +0300

    feat: update lib version

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.22", "commit":"82ca60dc8c80355f1fc5c997f476973c3fce7d21"}
+export const current_release={"release": "release-0.3.23", "commit":"0214cf83f37c5efa15ee3ba7d1155adacb44cd6c"}
