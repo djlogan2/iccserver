27ea93bfb84d2bd56228d18881d1063410116b9a
david_logan <PE4-fby-o2g-vMN>
2021-04-13 08:35:42 -0600

    remove board css

--- client/App.jsx
+++ client/App.jsx
@@ -42,7 +42,6 @@ export default compose(
     const lang = getLang();
 
     const subscriptions = {
-      css: Meteor.subscribe("css"),
       gameRequests: Meteor.subscribe("game_requests"),
       gameHistory: Meteor.subscribe("game_history"),
       clientInternationalization: Meteor.subscribe("clientInternationalization", lang)
--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.24-2-gad7386f", "commit":"ad7386f46653b72c2c1a786b4d0869aa05e5a8ff"}
+export const current_release={"release": "release-0.3.24-3-ge112db3", "commit":"e112db39ea2a22f8c1ee0b31e85e95c2964b84f6"}
