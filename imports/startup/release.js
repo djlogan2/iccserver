ada59443d3b69a884483665bf4bf0a9d07d89510
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-04-26 14:18:02 +0300

    fix: when user2 requests an adjourn from user1, notification is displayed eternally for user1

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.27", "commit":"c2cf191ad1d6d0859a17690667df2bc04ebde4b5"}
+export const current_release={"release": "release-0.3.27-2-g7dd926b", "commit":"7dd926b50b8090f5cb8b02a8a4703f8268e55b34"}
