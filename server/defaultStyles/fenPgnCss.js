32eda71bf74f2f4680d0e2fdbdad54bb48d5bf5b
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-05-19 11:28:40 +0300

    fix: add fen pgn css

---
+++ server/defaultStyles/fenPgnCss.js
@@ -0,0 +1,25 @@
+const fenPgnCss = {
+  type: "fenPgn",
+  main: {
+    padding: "0 2.4rem",
+    height: "calc(100% - 10px)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  content: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+  },
+  bottom: {
+    marginTop: "1.9rem",
+  },
+  button: {
+    background: "#1565C0",
+    border: "none",
+    marginRight: "1.8rem",
+  },
+};
+
+export default fenPgnCss;
