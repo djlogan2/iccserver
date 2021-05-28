d5a7f218ad8fa835b93fe2c56ea453dc9bc9d3ae
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-05-28 11:49:06 +0300

    fix: game request modal styles

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.29-109-g1649140", "commit":"16491407d0ce3dc1b041ea7b6b32c9af3f22e54d"}
+export const current_release={"release": "release-0.3.29-116-g0bc5003", "commit":"0bc5003acbcd84b8c7fe3725ef8917c4af5a2921"}
