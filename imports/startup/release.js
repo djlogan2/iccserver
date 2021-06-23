dbf71eb8f2bf64df12e1235762981a2b19f5f9cb
david_logan <PE4-fby-o2g-vMN>
2021-06-23 10:32:48 -0600

    Checking in some error handling in engine_manager.js

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.3.30-17-ga167fb0", "commit":"a167fb01e93772d6cdc4df554fcd6dff388338b6"}
+export const current_release={"release": "release-0.3.30-27-gf1f874d", "commit":"f1f874d356007bdc663e096ffd437f4d0f98dbf1"}
