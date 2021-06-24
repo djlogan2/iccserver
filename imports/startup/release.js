8edd3402ca66661247ec673d8739233f0d8ba895
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-06-24 17:24:55 +0300

    feat: add more tests; previous version of lib

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.4.0", "commit":"21563c8eaeac45c3f93ff5fa2f5724ad2acaac79"}
+export const current_release={"release": "release-0.4.0-4-g9b11fed", "commit":"9b11fed9f59d62221a8cf97273cfbd048fd951d2"}
