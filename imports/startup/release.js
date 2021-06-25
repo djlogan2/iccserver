9134fe40931e55e5522a396596bf77101e9306b0
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-06-25 17:43:20 +0300

    fix: smith on the game history schema

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.4.0-5-g8edd340", "commit":"8edd3402ca66661247ec673d8739233f0d8ba895"}
+export const current_release={"release": "release-0.4.0-7-gc35f577", "commit":"c35f577445c33a378ea2506d97630027cd5b01b8"}
