eadc3ff9327d69ccb91686131cb391e891d150d8
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-06-29 15:42:14 +0300

    fix: player clock after the game ends shows zero

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.4.0-7-gc35f577", "commit":"c35f577445c33a378ea2506d97630027cd5b01b8"}
+export const current_release={"release": "release-0.4.0-11-g8975a5d", "commit":"8975a5df7a71fa37027060bde86ecbf4e730cb0c"}
