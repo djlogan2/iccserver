cf78850f8110cd01c1e05adc5bfe1e6d9daaca45
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-08-09 18:40:19 +0300

    fix: add headers for slack notifications

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.4.2-2-gb41724a", "commit":"b41724a0b5c25323fc900e5aee2286821096d7b4"}
+export const current_release={"release": "release-0.4.2-3-gaffe978", "commit":"affe978ef7b74ae738cd7b2826387d3b3048a77a"}
