335aada5a59419ac57042f997aec8981249dc006
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-08-10 18:47:13 +0300

    fix: pgn import issue

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.4.2-9-gee1efca2", "commit":"ee1efca20cd50b834cedb03ebce7b19a5d7d215c"}
+export const current_release={"release": "release-0.4.2-10-g0ad173ab", "commit":"0ad173ab1bf7fedb172a3035030f6045ea571ee8"}
