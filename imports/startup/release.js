0569f56c527eeecd49c7901f32f95fc44b307740
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-08-10 15:51:48 +0300

    fix: not sending errors on local machine

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.4.2-5-gd19a1be0", "commit":"d19a1be0b4568f29fc2cbf2433d95ea5bcbde3b1"}
+export const current_release={"release": "release-0.4.2-6-gc80f1ad2", "commit":"c80f1ad2e353cec98eeecbfffcd2b0804e36caf8"}
