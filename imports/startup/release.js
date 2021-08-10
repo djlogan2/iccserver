ee1efca20cd50b834cedb03ebce7b19a5d7d215c
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-08-10 15:57:43 +0300

    fix: add callback for http request functions

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.4.2-6-gc80f1ad2", "commit":"c80f1ad2e353cec98eeecbfffcd2b0804e36caf8"}
+export const current_release={"release": "release-0.4.2-8-g207f9ab1", "commit":"207f9ab1dda4a22c0268eb8fad6268f0e60578f0"}
