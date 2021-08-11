c63e0cfa5b4619eb7425443c384c5af8fe62fc98
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-08-11 14:20:00 +0300

    fix: premove issue

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.4.2-10-g0ad173ab", "commit":"0ad173ab1bf7fedb172a3035030f6045ea571ee8"}
+export const current_release={"release": "release-0.4.2-11-g335aada5", "commit":"335aada5a59419ac57042f997aec8981249dc006"}
