83ce30d694a17d2010f3cd27a8d69d37f09169bd
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-09-10 14:47:55 +0300

    feat: add sound for the piece move (hardcoded); update meteor version

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.4.2-57-ga2c6384f", "commit":"a2c6384f642694a0bfe11675f8d3915860e903ec"}
+export const current_release={"release": "release-0.4.2-58-ga8bd8ad3", "commit":"a8bd8ad39d7909076e22a5ef678a17d408928c18"}
