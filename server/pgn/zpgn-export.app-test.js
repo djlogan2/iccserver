eb35b37543cdff83c75a9193753b8b4d083fa095
Aleksandr Dubovskiy <aleksandr.dubovskiy@gmail.com>
2021-09-15 17:10:14 +0300

    fix export pgn, update test with spaces

--- server/pgn/zpgn-export.app-test.js
+++ server/pgn/zpgn-export.app-test.js
@@ -45,7 +45,6 @@ describe("PGN exports", function() {
   it("should pass this test, which is failing in production", function() {
     const game_id = Game.collection.insert(game_record);
     const exported_pgn = Game.exportToPGN(game_id);
-    console.log(exported_pgn);
     //chai.assert.equal(exported_pgn, "");
   });
 
