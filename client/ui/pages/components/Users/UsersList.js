d5f200c2b5363e2942a44da5392afaa4beb784b8
oleshkoo-clockwise <aleksandr.oleshko@clockwise.software>
2021-09-30 18:19:00 +0300

    fix: remove isolation group column on the users list

--- client/ui/pages/components/Users/UsersList.js
+++ client/ui/pages/components/Users/UsersList.js
@@ -4,19 +4,12 @@ import { withRouter } from "react-router-dom";
 import { compose } from "redux";
 
 import AppWrapper from "../AppWrapper/AppWrapper";
-import {
-  renderRating,
-  renderStatus,
-  renderOnline,
-  renderButtonEdit,
-  renderEmail,
-} from "./renderListUtils";
+import { renderButtonEdit, renderEmail, renderOnline, renderRating, renderStatus } from "./renderListUtils";
 import { translate } from "../../../HOCs/translate";
 import injectSheet from "react-jss";
 import { withTracker } from "meteor/react-meteor-data";
 import { mongoCss } from "../../../../../imports/api/client/collections";
 import { dynamicUserManagementStyles } from "./dynamicUserManagementStyles";
-import { ROLE_LIST_USERS } from "../../../../constants/systemConstants";
 import { RESOURCE_USERS } from "../../../../constants/resourceConstants";
 
 const { Column, ColumnGroup } = Table;
@@ -51,14 +44,6 @@ class UsersList extends Component {
     const { history, translate, classes, roles } = this.props;
     const { usersList } = this.state;
 
-    const scope = roles.find((element) => {
-      if (element?.role?.id === ROLE_LIST_USERS) {
-        return !!element.scope ? element.scope : null;
-      }
-
-      return false;
-    });
-
     return (
       <AppWrapper>
         <div className={classes.listMainDiv}>
@@ -75,13 +60,6 @@ class UsersList extends Component {
               <Column title={translate("userInfo")} dataIndex="username" key="username" />
               <Column title={translate("email")} render={renderEmail} key="email" />
               <Column title={translate("locale")} dataIndex="locale" key="locale" />
-              {!scope && (
-                <Column
-                  title={translate("isolationGroup")}
-                  dataIndex="isolation_group"
-                  key="isolation_group"
-                />
-              )}
             </ColumnGroup>
             <ColumnGroup title={translate("ratings")}>
               <Column title={translate("blitz")} key="blitz" render={renderRating("blitz")} />
