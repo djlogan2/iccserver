eda018470ce2ea5ff2780f6304ede6864ac54ac7
djlogan2 <djlogan@chessclub.com>
2022-08-29 13:23:33 -0600

    More updates in case we have a future release.

--- imports/startup/release.js
+++ imports/startup/release.js
@@ -1,1 +1,1 @@
-export const current_release={"release": "release-0.6.2-3-gdcbb5643", "commit":"dcbb5643506825beebbfeec108876e53251f327f"}
+export const current_release={"release": "release-0.6.6", "commit":"2af24d0b362673f89d79f3a0968b5ff483ec8599"}
